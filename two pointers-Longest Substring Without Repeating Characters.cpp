33a530a4fb200e3db2b7c092bf632043a2ecf242
lixin <bangerlee@gmail.com>
2019-01-03 21:27:47 +0800

    Create two pointers-Longest Substring Without Repeating Characters.cpp

---
+++ two pointers-Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char,int> m;
+        int i=0,res=0;
+        for(int j=0;j<s.length();j++){
+            if(m[s[j]]++){
+                res=max(res,j-i);
+                while(i<j&&s[i]!=s[j]){
+                    m[s[i++]]--;
+                }
+                m[s[j]]--;
+                i++;
+            }
+            else res=max(res,j-i+1);
+        }
+        return res;
+    }
+};
