d9b821b882a07869e29459dc7b8000ebf52f47a5
lixin <bangerlee@gmail.com>
2019-01-03 21:33:24 +0800

    Create two pointers-Container With Most Water.cpp

---
+++ two pointers-Container With Most Water.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0,j=height.size()-1,res=0;
+        while(i<j){
+            int h=min(height[i],height[j]);
+            res=max(res,h*(j-i));
+            while(i<j&&height[i]<=h) i++;
+            while(i<j&&height[j]<=h) j--;
+        }
+        return res;
+    }
+};
