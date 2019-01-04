6b441a4dcb16c67a209603911a700a4042e7a7d9
lixin <bangerlee@gmail.com>
2019-01-04 18:01:15 +0800

    Create two pointers-K-diff Pairs in an Array.cpp

---
+++ two pointers-K-diff Pairs in an Array.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/k-diff-pairs-in-an-array/
+
+Runtime: 32 ms
+*/
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0,res=0;
+        for(int j=1;j<nums.size();){
+            if(nums[j]-nums[i]<k||i>=j) j++;
+            else if(i>0&&nums[i]==nums[i-1]||(nums[j]-nums[i]>k)) i++;
+            else
+                res++,i++;
+        }
+        return res;
+    }
+};
