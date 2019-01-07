f56039ebe23f0d40fa747455f27e5055bb3535d7
lixin <bangerlee@gmail.com>
2019-01-07 21:24:36 +0800

    Create Insert Interval.cpp

---
+++ sort/Insert Interval.cpp
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/insert-interval/
+
+*/
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        vector<Interval> res;
+        int index = 0;
+        while(index < intervals.size() && intervals[index].end < newInterval.start){
+            res.push_back(intervals[index++]);
+        }
+        while(index < intervals.size() && intervals[index].start <= newInterval.end){
+            newInterval.start = min(newInterval.start, intervals[index].start);
+            newInterval.end = max(newInterval.end, intervals[index].end);
+            index++;
+        }
+        res.push_back(newInterval);
+        while(index < intervals.size()){
+            res.push_back(intervals[index++]);
+        }
+        return res;
+    }
+};
