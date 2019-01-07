2d47b135fb3c322654e87d80e93ae1bac69ea871
lixin <bangerlee@gmail.com>
2019-01-07 21:22:09 +0800

    Create Insertion Sort List.cpp

---
+++ sort/Insertion Sort List.cpp
@@ -0,0 +1,50 @@
+/*
+https://leetcode.com/problems/insertion-sort-list/
+
+Runtime: 44 ms
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(head==NULL||head->next==NULL) return head;
+        //ListNode* phead=head;
+        ListNode* p=head->next;
+        //ListNode* a=head;
+        //a->next=NULL;
+        head->next=NULL;
+        while(p!=NULL){
+            ListNode* phead=head;
+            ListNode* pr=NULL;
+            while(phead!=NULL && p->val>=phead->val)
+            {
+                pr=phead;
+                phead=phead->next;
+            }
+            if(phead==head){
+                phead=p;
+                p=p->next;
+                phead->next=head;
+                head=phead;
+            }
+            else if(phead==NULL){
+                pr->next=p;
+                p=p->next;
+                pr->next->next=NULL;
+            }
+            else{
+                pr->next=p;
+                p=p->next;
+                pr->next->next=phead;
+            }
+        }
+        return head;
+    }
+};
