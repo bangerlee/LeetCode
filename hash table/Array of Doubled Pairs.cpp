a5998013084344267ccb1b3e7419aa67a5b2d476
lixin <bangerlee@gmail.com>
2019-01-20 16:31:03 +0800

    Create Array of Doubled Pairs.cpp

---
+++ hash table/Array of Doubled Pairs.cpp
@@ -0,0 +1,20 @@
+/*
+https://leetcode.com/problems/array-of-doubled-pairs/
+*/
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& A) {
+        unordered_map<int,int> m;
+        for(int x:A) m[x]++;
+        vector<int> k;
+        for(auto x:m) k.push_back(x.first);
+        sort(k.begin(),k.end(),[](int a,int b){
+            return abs(a)<abs(b);
+        });
+        for(int x:k){
+            if(m[x]>m[2*x]) return false;
+            m[2*x]-=m[x];
+        }
+        return true;
+    }
+};
