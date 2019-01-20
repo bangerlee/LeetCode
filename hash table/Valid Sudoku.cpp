7bf82982d8defba61d428b3c2f55316c35656d94
lixin <bangerlee@gmail.com>
2019-01-20 16:26:22 +0800

    Create Valid Sudoku.cpp

---
+++ hash table/Valid Sudoku.cpp
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/valid-sudoku/
+*/
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        set<string> ss;
+        for(int i=0;i<9;i++){
+            string si=to_string(i);
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                string b="("+string(1,board[i][j])+")";
+                string r=b+si;
+                string c=to_string(j)+b;
+                string h=to_string(int(i/3))+b+to_string(int(j/3));
+                if(ss.count(r)||ss.count(c)||ss.count(h)) return false;
+                ss.insert(r);
+                ss.insert(c);
+                ss.insert(h);
+            }
+        }
+        return true;
+    }
+};
