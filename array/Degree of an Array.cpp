da24bb5a75392f148646bbebbe20d8ecf81d1fa2
lixin <bangerlee@gmail.com>
2019-01-28 18:53:49 +0800

    Update Degree of an Array.cpp

--- array/Degree of an Array.cpp
+++ array/Degree of an Array.cpp
@@ -1,11 +1,17 @@
 /*
 https://leetcode.com/problems/degree-of-an-array/
 
+loop only once on array {
+    record the index of first occureence;
+    update number counter;
+    update result;
+}
+return result;
 */
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        map<int,int> counter,first;
+        unordered_map<int,int> counter,first;
         int res=INT_MAX,degree=0;
         for(int i=0;i<nums.size();i++){
             if(first.count(nums[i])==0) first[nums[i]]=i;
