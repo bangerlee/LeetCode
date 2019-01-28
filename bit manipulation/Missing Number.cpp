b27a70e8b104d20951ce2d997a23fe84de5fd6c0
lixin <bangerlee@gmail.com>
2019-01-28 19:12:16 +0800

    Create Missing Number.cpp

---
+++ bit manipulation/Missing Number.cpp
@@ -0,0 +1,15 @@
+/*
+https://leetcode.com/problems/missing-number/
+
+*/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res=nums.size();
+        for(int i=0;i<nums.size();i++){
+            res^=i;
+            res^=nums[i];
+        }
+        return res;
+    }
+};
