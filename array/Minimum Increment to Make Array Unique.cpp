ecc53c3913194778d72d4ab5ad766c64179e3d64
lixin <bangerlee@gmail.com>
2019-01-30 11:42:07 +0800

    Create Minimum Increment to Make Array Unique.cpp

---
+++ array/Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+
+*/
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& A) {
+        sort(A.begin(),A.end());
+        int res=0,need=0;
+        for(int x:A){
+            res+=max(need-x,0);
+            need=max(need,x)+1;
+        }
+        return res;
+    }
+};
