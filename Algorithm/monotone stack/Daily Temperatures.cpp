2f9a9f87976ecc775c3e296e713f7a67c86d644f
lixin <bangerlee@gmail.com>
2019-01-30 20:05:24 +0800

    Create Daily Temperatures.cpp

---
+++ Algorithm/monotone stack/Daily Temperatures.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/daily-temperatures/
+
+*/
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        stack<int> monostack;
+        vector<int> res(T.size(),0);
+        for(int i=0;i<T.size();i++){
+            while(!monostack.empty()&&T[i]>T[monostack.top()]){
+                res[monostack.top()]=i-monostack.top();
+                monostack.pop();
+            }
+            monostack.push(i);
+        }
+        return res;
+    }
+};
