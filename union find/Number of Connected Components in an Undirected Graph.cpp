5237f3e2f230d4e8a4728bb2ce585e1e3b546cc1
lixin <bangerlee@gmail.com>
2019-03-11 21:41:48 +0800

    Create Number of Connected Components in an Undirected Graph.cpp

---
+++ union find/Number of Connected Components in an Undirected Graph.cpp
@@ -0,0 +1,26 @@
+/*
+https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+
+*/
+class Solution {
+private:
+    int find(vector<int>& nums,int i){
+        while(nums[i]!=i){
+            nums[i]=nums[nums[i]];
+            i=nums[i];
+        }
+        return i;
+    }
+public:
+    int countComponents(int n, vector<pair<int, int>>& edges) {
+        vector<int> nums(n);
+        for(int i=0;i<n;i++) nums[i]=i;
+        int res=n;
+        for(auto edge:edges){
+            int x=find(nums,edge.first);
+            int y=find(nums,edge.second);
+            if(x!=y){ nums[x]=y;res--;}
+        }
+        return res;
+    }
+};
