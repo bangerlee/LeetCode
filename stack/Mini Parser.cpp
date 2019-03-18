0d3d8028e8cadc14ff6ec49fc1ee6aa263637706
lixin <bangerlee@gmail.com>
2019-03-18 12:44:51 +0800

    Create Mini Parser.cpp

---
+++ stack/Mini Parser.cpp
@@ -0,0 +1,60 @@
+/*
+https://leetcode.com/problems/mini-parser/
+
+*/
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        function<bool(char)> isnumber=[](char c){ return c=='-'||isdigit(c);};
+        stack<NestedInteger> st;
+        st.push(NestedInteger());
+        for(auto it=s.begin();it!=s.end();){
+            const char & c=(*it);
+            if((isnumber(c))){
+                auto it2=find_if_not(it,s.end(),isnumber);
+                int val=stoi(string(it,it2));
+                st.top().add(NestedInteger(val));
+                it=it2;
+            }
+            else{
+                if(c=='[') st.push(NestedInteger());
+                else if(c==']'){
+                    NestedInteger ni=st.top();
+                    st.pop();
+                    st.top().add(ni);
+                }
+                ++it;
+            }
+        }
+        return st.top().getList().front();
+    }
+};
