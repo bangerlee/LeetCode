0a4550486c2d406885327ef753529e38394ccbc8
lixin <bangerlee@gmail.com>
2019-04-14 22:17:46 +0800

    Create Power of Two.cpp

---
+++ bit manipulation/Power of Two.cpp
@@ -0,0 +1,11 @@
+/*
+https://leetcode.com/problems/power-of-two/
+
+*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<1) return false;
+        return (n&(n-1))==0;
+    }
+};
