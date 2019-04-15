0f21786b655ee6cf9537f54a9f097ab9721d7ff0
lixin <bangerlee@gmail.com>
2019-04-15 09:37:18 +0800

    Create Single Number.cpp

---
+++ bit manipulation/Single Number.cpp
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/single-number-ii/submissions/
+
+*/
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+        for(auto x:nums) res^=x;
+        return res;
+    }
+};
