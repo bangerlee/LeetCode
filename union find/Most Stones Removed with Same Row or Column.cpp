3a5074f2b8d694d8a02c3310b3687a06d3707b18
lixin <bangerlee@gmail.com>
2019-04-20 15:03:45 +0800

    Create Most Stones Removed with Same Row or Column.cpp

---
+++ union find/Most Stones Removed with Same Row or Column.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+
+*/
+class Solution {
+private:
+    unordered_map<int,int> f;
+    int islands=0;
+    int find(int x){
+        if(!f.count(x)) f[x]=x,islands++;
+        if(x!=f[x]) f[x]=find(f[x]);
+        return f[x];
+    }
+    void uni(int x,int y){
+        x=find(x),y=find(y);
+        if(x!=y) f[x]=y,islands--;
+    }
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        for(int i=0;i<stones.size();i++){
+            uni(stones[i][0],~stones[i][1]);
+        }
+        return stones.size()-islands;
+    }
+};
