3eb423c08f0c34e2318bf85ea493983ce86a5ab4
lixin <bangerlee@gmail.com>
2019-04-22 12:00:13 +0800

    Create Longest Increasing Subsequence.cpp

---
+++ dp/Longest Increasing Subsequence.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/longest-increasing-subsequence/
+
+*/
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        vector<int> dp(nums.size());
+        dp[0]=1;
+        for(int i=1;i<nums.size();i++){
+            dp[i]=1;
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]&&dp[i]<dp[j]+1) dp[i]=dp[j]+1;
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
