80556b61ccad4d446a507d79887be8157254a52f
lixin <bangerlee@gmail.com>
2019-08-02 14:51:50 +0800

    Create Powerful Integers.cpp

---
+++ hash table/Powerful Integers.cpp
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/powerful-integers/
+
+*/
+class Solution {
+public:
+    vector<int> powerfulIntegers(int x, int y, int bound) {
+        unordered_set<int> s;
+        for(int i=1;i<=bound;i*=x){
+            for(int j=1;j+i<=bound;j*=y){
+                s.insert(i+j);
+                if(y==1) break;
+            }
+            if(x==1) break;
+        }
+        return vector<int>(s.begin(),s.end());
+    }
+};
