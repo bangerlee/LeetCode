85d409288fe0217c6f9d9340ba0995c2e0542752
lixin <bangerlee@gmail.com>
2019-08-02 11:29:23 +0800

    Update Number of Boomerangs.cpp

--- hash table/Number of Boomerangs.cpp
+++ hash table/Number of Boomerangs.cpp
@@ -4,19 +4,18 @@ https://leetcode.com/problems/number-of-boomerangs/
 */
 class Solution {
 public:
-    int numberOfBoomerangs(vector<pair<int, int>>& points) {
+    int numberOfBoomerangs(vector<vector<int>>& points) {
         int res=0;
         for(int i=0;i<points.size();i++){
             unordered_map<long,int> m;
             for(int j=0;j<points.size();j++){
                 if(i==j) continue;
-                int dx=points[j].first-points[i].first;
-                int dy=points[j].second-points[i].second;
+                int dx=points[j][0]-points[i][0];
+                int dy=points[j][1]-points[i][1];
                 long k=dx*dx+dy*dy;
                 ++m[k];
             }
-            for(auto p:m)
-                res+=p.second*(p.second-1);
+            for(auto& p:m) res+=p.second*(p.second-1);
         }
         return res;
     }
