af1328351ad460fecf056f201497248b86c971d8
lixin <bangerlee@gmail.com>
2019-08-02 15:55:07 +0800

    Update Isomorphic Strings.cpp

--- hash table/Isomorphic Strings.cpp
+++ hash table/Isomorphic Strings.cpp
@@ -5,23 +5,13 @@ https://leetcode.com/problems/isomorphic-strings/
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,int> m;
-        unordered_map<char,int> mt;
-        for(int i=0;i<s.length();i++){
-            unordered_map<char,int>::iterator it=m.find(s[i]);
-            unordered_map<char,int>::iterator itt=mt.find(t[i]);
-            if((it==m.end() && itt!=mt.end()) || (it!=m.end() && itt==mt.end()))
-                return false;
-            else if(it==m.end()&&itt==mt.end()){
-                m[s[i]]=i;
-                mt[t[i]]=i;
-            }
-            else{
-                if(it->second!=itt->second)
-                    return false;
-                m[s[i]]=i;
-                mt[t[i]]=i;
-            }
+        vector<int> m1(256,0);
+        vector<int> m2(256,0);
+        int n=t.length();
+        for(int i=0;i<n;i++){
+            if(m1[s[i]]!=m2[t[i]]) return false;
+            m1[s[i]]=i+1;
+            m2[t[i]]=i+1;
         }
         return true;
     }
