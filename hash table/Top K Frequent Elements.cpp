3b348167fcfa3472e8ff29d02f7ad322b50872f7
lixin <bangerlee@gmail.com>
2019-08-03 14:49:52 +0800

    Update Top K Frequent Elements.cpp

--- hash table/Top K Frequent Elements.cpp
+++ hash table/Top K Frequent Elements.cpp
@@ -1,24 +1,24 @@
 /*
 https://leetcode.com/problems/top-k-frequent-elements/
+
 */
 class Solution {
+private:
+    struct compare{
+        bool operator()(const vector<int> a,const vector<int> b){
+            return b[0]>a[0];
+        }  
+    };
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> m;
-        map<int,vector<int>> mv;
         vector<int> res;
-        for(int x:nums) m[x]++;
-        auto it=m.begin();
-        while(it!=m.end()){
-            mv[it->second].push_back(it->first);
-            it++;
-        }
-        auto p=mv.end();
-        p--;
-        while(res.size()<k){
-            for(int j=0;j<p->second.size()&&res.size()<k;j++)
-                res.push_back(p->second[j]);
-            p--;
+        priority_queue<vector<int>,vector<vector<int>>,compare> pq;
+        unordered_map<int,int> m;
+        for(auto& x:nums) m[x]++;
+        for(auto& k:m) pq.push({k.second,k.first});
+        for(int i=0;i<k;i++){
+            res.push_back(pq.top()[1]);
+            pq.pop();
         }
         return res;
     }
