e0191189766f15ce590dd4d2b2c5ab5a4a9573b2
lixin <bangerlee@gmail.com>
2019-08-03 13:59:08 +0800

    Update Word Pattern.cpp

--- hash table/Word Pattern.cpp
+++ hash table/Word Pattern.cpp
@@ -2,15 +2,19 @@
 https://leetcode.com/problems/word-pattern/
 
 */
-bool wordPattern(string pattern, string str) {
-    map<char, int> p2i;
-    map<string, int> w2i;
-    istringstream in(str);
-    int i = 0, n = pattern.size();
-    for (string word; in >> word; ++i) {
-        if (i == n || p2i[pattern[i]] != w2i[word])
-            return false;
-        p2i[pattern[i]] = w2i[word] = i + 1;
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        unordered_map<char,int> p2i;
+        unordered_map<string,int> w2i;
+        istringstream in(str);
+        int i=0,n=pattern.size();
+        string word;
+        while(in>>word){
+            if(i==n||p2i[pattern[i]]!=w2i[word]) return false;
+            p2i[pattern[i]]=w2i[word]=i+1;
+            ++i;
+        }
+        return i==n;
     }
-    return i == n;
-}
+};
