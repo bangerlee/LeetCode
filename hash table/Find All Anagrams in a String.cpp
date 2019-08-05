b18637d31559f900444620a85864d30c45db2597
lixin <bangerlee@gmail.com>
2019-08-05 13:24:07 +0800

    Update Find All Anagrams in a String.cpp

--- hash table/Find All Anagrams in a String.cpp
+++ hash table/Find All Anagrams in a String.cpp
@@ -5,35 +5,14 @@ https://leetcode.com/problems/find-all-anagrams-in-a-string/
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        
-        map<char,int> m;
-        map<char,int> mt;
+        unordered_map<char,int> m;
+        int begin=0,end=0,cnt=p.length();
+        for(auto& c:p) m[c]++;
         vector<int> res;
-        if(s.length()<p.length())
-            return res;
-        for(int i=0;i<p.length();i++){
-            if(m.find(p[i])==m.end())
-                m[p[i]]=1;
-            else
-                m[p[i]]++;
-        }
-        int i=0;
-        for(;i<p.length();i++){
-            if(mt.find(s[i])==mt.end())
-                mt[s[i]]=1;
-            else
-                mt[s[i]]++;
-        }
-        if(mt==m) res.push_back(0);
-        for(;i<s.length();i++){
-            if(mt.find(s[i])==mt.end())
-                mt[s[i]]=1;
-            else
-                mt[s[i]]++;
-            mt[s[i-p.length()]]--;
-            if(mt[s[i-p.length()]]==0)
-                mt.erase(s[i-p.length()]);
-            if(mt==m) res.push_back(i-p.length()+1);
+        while(end<s.length()){
+            if(m[s[end++]]-->0) cnt--;
+            if(cnt==0) res.push_back(begin);
+            if(end-begin>=p.length()&&++m[s[begin++]]>0) cnt++;
         }
         return res;
     }
