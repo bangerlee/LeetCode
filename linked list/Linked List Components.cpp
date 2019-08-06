2f9808ae5bca22e5056614846391a1cc248c2607
lixin <bangerlee@gmail.com>
2019-08-06 15:17:48 +0800

    Update Linked List Components.cpp

--- linked list/Linked List Components.cpp
+++ linked list/Linked List Components.cpp
@@ -1,7 +1,6 @@
 /*
 https://leetcode.com/problems/linked-list-components/
 
-Runtime: 352 ms
 */
 /**
  * Definition for singly-linked list.
@@ -14,17 +13,12 @@ Runtime: 352 ms
 class Solution {
 public:
     int numComponents(ListNode* head, vector<int>& G) {
+        unordered_set<int> setG(G.begin(),G.end());
         int res=0;
-        ListNode* p=head;
-        while(p!=NULL){
-            if(find(G.begin(),G.end(),p->val)!=G.end()){
-                while(p!=NULL&&find(G.begin(),G.end(),p->val)!=G.end()){
-                    p=p->next;
-                }
-                res+=1;
-            }
-            if(p!=NULL)
-                p=p->next;
+        while(head!=NULL){
+            if(setG.count(head->val)&&(head->next==NULL||!setG.count(head->next->val)))
+                res++;
+            head=head->next;
         }
         return res;
     }
