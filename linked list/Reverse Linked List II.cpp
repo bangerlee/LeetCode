ba31021802079ce1ddef946d6d43536615895db5
lixin <bangerlee@gmail.com>
2019-08-06 11:51:16 +0800

    Update Reverse Linked List II.cpp

--- linked list/Reverse Linked List II.cpp
+++ linked list/Reverse Linked List II.cpp
@@ -2,21 +2,27 @@
 https://leetcode.com/problems/reverse-linked-list-ii/
 
 */
-class Solution {  
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-        ListNode* new_head = new ListNode(0);
-        new_head -> next = head;
-        ListNode* pre = new_head;
-        for (int i = 0; i < m - 1; i++)
-            pre = pre -> next;
-        ListNode* cur = pre -> next;
-        for (int i = 0; i < n - m; i++) {
-            ListNode* move = cur -> next; 
-            cur -> next = move -> next;
-            move -> next = pre -> next;
-            pre -> next = move;
+        ListNode* dummy=new ListNode(0), *pre=dummy, *cur;
+        dummy->next=head;
+        for(int i=0;i<m-1;i++) pre=pre->next;
+        cur=pre->next;
+        for(int i=0;i<n-m;i++){
+            ListNode* tmp=pre->next;
+            pre->next=cur->next;
+            cur->next=cur->next->next;
+            pre->next->next=tmp;
         }
-        return new_head -> next;
+        return dummy->next;
     }
 };
