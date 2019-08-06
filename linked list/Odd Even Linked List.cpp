ff91ee8a66e8383baa618ad1d927549beca0b327
lixin <bangerlee@gmail.com>
2019-08-06 14:48:23 +0800

    Create Odd Even Linked List.cpp

---
+++ linked list/Odd Even Linked List.cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/odd-even-linked-list/
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL) return NULL;
+        ListNode* odd=head,*even=head->next,*evenhead=even;
+        while(even!=NULL&&even->next!=NULL){
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=evenhead;
+        return head;
+    }
+};
