191d0e09c9cd54878956a319d42c96faa6ad777f
lixin <bangerlee@gmail.com>
2019-08-06 13:03:15 +0800

    Update Insertion Sort List.cpp

--- linked list/Insertion Sort List.cpp
+++ linked list/Insertion Sort List.cpp
@@ -11,37 +11,29 @@ Runtime: 44 ms
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        if(head==NULL||head->next==NULL) return head;
-        ListNode* p=head->next;
-        head->next=NULL;
-        while(p!=NULL){
-            ListNode* phead=head;
-            ListNode* pr=NULL;
-            while(phead!=NULL && p->val>=phead->val)
-            {
-                pr=phead;
-                phead=phead->next;
-            }
-            if(phead==head){
-                phead=p;
-                p=p->next;
-                phead->next=head;
-                head=phead;
-            }
-            else if(phead==NULL){
-                pr->next=p;
-                p=p->next;
-                pr->next->next=NULL;
-            }
-            else{
-                pr->next=p;
-                p=p->next;
-                pr->next->next=phead;
-            }
+        ListNode* dummy=new ListNode(0);
+        ListNode* cur=head;
+        ListNode* prev=dummy;
+        ListNode* next=NULL;
+        while(cur){
+            next=cur->next;
+            if(!prev||!prev->next||prev->next->val>=cur->val) prev=dummy;
+            while(prev->next&&prev->next->val<cur->val) prev=prev->next;
+            cur->next=prev->next;
+            prev->next=cur;
+            cur=next;
         }
-        return head;
+        return dummy->next;
     }
 };
