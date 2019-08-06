dd25bdb5a8958df8b75c22c1151e28e0cb041a37
lixin <bangerlee@gmail.com>
2019-08-06 14:50:31 +0800

    Create Next Greater Node In Linked List.cpp

---
+++ linked list/Next Greater Node In Linked List.cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/next-greater-node-in-linked-list/
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> res;
+        stack<int> st;
+        for(auto p=head;p!=NULL;p=p->next) res.push_back(p->val);
+        for(int i=res.size()-1;i>=0;i--){
+            int val=res[i];
+            while(!st.empty() && st.top()<=res[i]) st.pop();
+            res[i]=st.empty()?0:st.top();
+            st.push(val);
+        }
+        return res;
+    }
+};
