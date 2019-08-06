72c48d7f456b8dd4221899f8a42535e2e282ee24
lixin <bangerlee@gmail.com>
2019-08-06 13:33:25 +0800

    Update Swap Nodes in Pairs.cpp

--- linked list/Swap Nodes in Pairs.cpp
+++ linked list/Swap Nodes in Pairs.cpp
@@ -1,7 +1,6 @@
 /*
 https://leetcode.com/problems/swap-nodes-in-pairs/
 
-Runtime: 4 ms
 */
 /**
  * Definition for singly-linked list.
@@ -14,20 +13,10 @@ Runtime: 4 ms
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL) return head;
-        if(head->next==NULL) return head;
-        ListNode* nhead=head->next;
-        ListNode* e=head;
-        ListNode* b=nhead;
-        do{
-            ListNode* tmp=b->next;
-            b->next=e;
-            e->next=tmp;
-            if(tmp==NULL) break;
-            b=tmp->next;
-            if(b!=NULL) e->next=b;
-            e=tmp;
-        }while(b!=NULL);
-        return nhead;
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* p=head->next;
+        head->next=swapPairs(head->next->next);
+        p->next=head;
+        return p;
     }
 };
