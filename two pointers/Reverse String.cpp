3b1ed4e673b7135ee304c98637cb00c0a7cb5184
lixin <bangerlee@gmail.com>
2019-08-14 15:42:53 +0800

    Create Reverse String.cpp

---
+++ two pointers/Reverse String.cpp
@@ -0,0 +1,11 @@
+/*
+https://leetcode.com/problems/reverse-string/
+
+*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0,j=s.size()-1;
+        while(i<j) swap(s[i++],s[j--]);
+    }
+};
