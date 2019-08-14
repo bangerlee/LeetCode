c73032fc02c2680e9ad558c2dcbd600bb2b1f50f
lixin <bangerlee@gmail.com>
2019-08-14 17:20:35 +0800

    Update Intersection of Two Arrays II.cpp

--- two pointers/Intersection of Two Arrays II.cpp
+++ two pointers/Intersection of Two Arrays II.cpp
@@ -27,3 +27,21 @@ Processing the Strings by chunk, which fits the memory, then deal with each chun
 
 Processing the Strings by streaming, then check.
 */
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int> res;
+        int i1=0,i2=0;
+        while(i1<nums1.size()&&i2<nums2.size()){
+            if(nums1[i1]==nums2[i2]){
+                res.push_back(nums1[i1]);
+                i1++,i2++;
+            }
+            else if(nums1[i1]>nums2[i2]) i2++;
+            else i1++;
+        }
+        return res;
+    }
+};
