baaf47a8afbc5cd740c78da66ea71340ac4e4b91
lixin <bangerlee@gmail.com>
2019-08-14 16:59:17 +0800

    Create Rotate List.cpp

---
+++ two pointers/Rotate List.cpp
@@ -0,0 +1,31 @@
+/*
+https://leetcode.com/problems/rotate-list/
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL) return NULL;
+        int len=1;
+        ListNode* newHead,*tail;
+        newHead=tail=head;
+        while(tail->next){
+            tail=tail->next;
+            len++;
+        }
+        tail->next=head;
+        if(k%=len)
+            for(int i=0;i<len-k;i++) tail=tail->next;
+        newHead=tail->next;
+        tail->next=NULL;
+        return newHead;
+    }
+};
