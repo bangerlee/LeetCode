f6924259d28dc84a7c355ffc5690d0b2dfdd37ec
lixin <bangerlee@gmail.com>
2019-08-14 15:35:04 +0800

    Create Linked List Cycle.cpp

---
+++ two pointers/Linked List Cycle.cpp
@@ -0,0 +1,26 @@
+/*
+https://leetcode.com/problems/linked-list-cycle/
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL) return false;
+        ListNode* p=head;
+        ListNode* pp=head;
+        while(pp->next!=NULL&&pp->next->next!=NULL){
+            p=p->next;
+            pp=pp->next->next;
+            if(p==pp) return true;
+        }
+        return false;
+    }
+};
