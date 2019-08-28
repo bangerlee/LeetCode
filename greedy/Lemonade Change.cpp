554595fac4bb5f70c1bf76251855432bbf111d9d
lixin <bangerlee@gmail.com>
2019-08-28 10:55:07 +0800

    Create Lemonade Change.cpp

---
+++ greedy/Lemonade Change.cpp
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/lemonade-change/
+
+*/
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0;
+        for(auto i:bills){
+            if(i==5) five++;
+            else if(i==10) ten++,five--;
+            else if(ten>0) ten--,five--;
+            else five-=3;
+            if(five<0) return false;
+        }
+        return true;
+    }
+};
