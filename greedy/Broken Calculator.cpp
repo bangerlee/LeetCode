8ed62ff9edae680a6ec31482ee626efed5a1d99f
lixin <bangerlee@gmail.com>
2019-08-30 11:48:52 +0800

    Create Broken Calculator.cpp

---
+++ greedy/Broken Calculator.cpp
@@ -0,0 +1,15 @@
+/*
+https://leetcode.com/problems/broken-calculator/
+
+*/
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int res=0;
+        while(Y>X){
+            Y=Y%2>0?Y+1:Y/2;
+            res++;
+        }
+        return res+X-Y;
+    }
+};
