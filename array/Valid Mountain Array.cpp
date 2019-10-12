28bc48e011f834c8b48052ed356d299d4a796118
lixin <bangerlee@gmail.com>
2019-10-12 14:46:22 +0800

    Create Valid Mountain Array.cpp

---
+++ array/Valid Mountain Array.cpp
@@ -0,0 +1,13 @@
+/*
+https://leetcode.com/problems/valid-mountain-array/
+
+*/
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        int n=A.size(),i=0,j=n-1;
+        while(i+1<n&&A[i]<A[i+1]) ++i;
+        while(j>0&&A[j-1]>A[j]) --j;
+        return i>0&&i==j&&j<n-1;
+    }
+};
